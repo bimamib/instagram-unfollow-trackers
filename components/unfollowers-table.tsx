66997c35da226996d0620b4f6bf6452ac1988bc7
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-02-23 22:44:59 +0700

    Adding select item 100

--- components/unfollowers-table.tsx
+++ components/unfollowers-table.tsx
@@ -140,6 +140,7 @@ export function UnfollowersTable({
               <SelectItem value="10">10</SelectItem>
               <SelectItem value="20">20</SelectItem>
               <SelectItem value="50">50</SelectItem>
+              <SelectItem value="100">100</SelectItem>
             </SelectContent>
           </Select>
         </div>
