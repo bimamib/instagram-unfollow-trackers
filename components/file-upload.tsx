94f59515980bf0dc9a55d5e5f2d078a63880d303
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2025-10-26 17:36:06 +0700

    Enhance file input styling in FileUpload component for improved accessibility and visual consistency

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -166,11 +166,11 @@ export function FileUpload() {
         <Input
           id="followers"
           type="file"
-          className="block w-full border shadow-sm rounded-lg text-sm focus:z-10 disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400
-        file:bg-gray-50 file:border-0
-          file:me-4
-          file:py-2 file:px-4
-        dark:file:bg-neutral-700 dark:file:text-neutral-400"
+          className="block w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400
+      file:bg-gray-50 file:border-0
+      file:me-4
+      file:py-2 file:px-4
+      dark:file:bg-neutral-700 dark:file:text-neutral-400"
           onChange={(e) => setFollowersFile(e.target.files?.[0] || null)}
         />
       </div>
@@ -179,11 +179,11 @@ export function FileUpload() {
         <Input
           id="followings"
           type="file"
-          className="block w-full border shadow-sm rounded-lg text-sm focus:z-10 disabled:opacity-50 disabled:pointer-events-none dark:text-neutral-400
-        file:bg-gray-50 file:border-0
-          file:me-4
-          file:py-2 file:px-4
-        dark:file:bg-neutral-700 dark:file:text-neutral-400"
+          className="block w-full border border-gray-200 shadow-sm rounded-lg text-sm focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400
+      file:bg-gray-50 file:border-0
+      file:me-4
+      file:py-2 file:px-4
+      dark:file:bg-neutral-700 dark:file:text-neutral-400"
           onChange={(e) => setFollowingsFile(e.target.files?.[0] || null)}
         />
       </div>
